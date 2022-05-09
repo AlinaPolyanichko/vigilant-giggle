7cc0bb6823a07df0999a09924e4886049c495af3
AlinaPolyanichko <105213921+AlinaPolyanichko@users.noreply.github.com>
2022-05-09 14:58:44 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+document.addEventListener("keydown", function(piano) {
+    let keys  = ['A', 'S', 'D', 'F', 'G', 'H', 'J'];
+    let input = piano.key.toUpperCase();
+
+    if (keys.includes(input)) {
+        let audio = document.createElement("AUDIO");
+    }
+});
+document.addEventListener("keydown", function(piano) {
+    let keys  = ['W', 'E', 'T', 'Y', 'U'];
+    let input = piano.key.toUpperCase();
+
+    if (keys.includes(input)) {
+        let audio = document.createElement("AUDIO");
+    }
+});
+
+
+
